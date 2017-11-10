a1350d4fd01a82b11c2219afc62bd7b1f807ff13
dan <dancolyer@icloud.com>
2017-11-10 10:56:10 +0000

    Initialised and created classes for Homework

---
+++ app/src/main/java/com/example/daniel/zoomanager/Animal.java
@@ -0,0 +1,15 @@
+package com.example.daniel.zoomanager;
+
+/**
+ * Created by Daniel on 10/11/2017.
+ */
+
+public abstract class Animal {
+    public String name;
+    public double cash_value;
+
+    public Animal(String name, double cash_value) {
+        this.name = name;
+        this.cash_value = cash_value;
+    }
+}
---
+++ app/src/main/java/com/example/daniel/zoomanager/PolarBear.java
@@ -0,0 +1,8 @@
+package com.example.daniel.zoomanager;
+
+/**
+ * Created by Daniel on 10/11/2017.
+ */
+
+public class PolarBear {
+}
